2b6669cba27e20186d9ac377ec6b5f71d99897c1
jozimarback <jozimar.back@evosix.com.br>
2016-04-05 22:50:53 -0300

    Adicionado viewmodel cadastrorefeicao viewmodel e adicionado bidings nele para notificar o frontend.

--- NutricaoPortableApp/NutricaoPortableApp/View/CadastroRefeicao.xaml.cs
+++ NutricaoPortableApp/NutricaoPortableApp/View/CadastroRefeicao.xaml.cs
@@ -6,41 +6,20 @@ using System.Text;
 using System.Threading.Tasks;
 using NutricaoPortableApp.Data;
 using NutricaoPortableApp.Model;
+using NutricaoPortableApp.ViewModel;
 using Xamarin.Forms;
 
 namespace NutricaoPortableApp.View
 {
     public partial class CadastroRefeicao : ContentPage
     {
-        private RefeicaoDao _dao;
         public CadastroRefeicao(RefeicaoDao dao)
         {
-            this._dao = dao;
+            CadastroRefeicaoViewModel vm = new CadastroRefeicaoViewModel(dao,this);
+            BindingContext = vm;
             InitializeComponent();
         }
 
-        public void AtualizaContador(Object sender, EventArgs e)
-        {
-            double valor = StpCalorias.Value;
-            LblCalorias.Text = valor.ToString();
-        }
-
-        public void SalvaRefeicao(Object sender, EventArgs e)
-        {
-            string descricao = EntDescricao.Text;
-            double valor = StpCalorias.Value;
-            Refeicao refeicao = new Refeicao(descricao, valor);
-            _dao.Salvar(refeicao);
-            string msg = string.Format("A refeição {0} de {1} foi salva com successo.", descricao, valor);
-            DisplayAlert("Salvar refeição", msg, "Ok");
-            Clear();
-        }
-
-        public void Clear()
-        {
-            EntDescricao.Text = "";
-            StpCalorias.Value = 10;
-        }
 
         //private void MostraLista(object sender, EventArgs e)
         //{
---
+++ NutricaoPortableApp/NutricaoPortableApp/ViewModel/CadastroRefeicaoViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using NutricaoPortableApp.Data;
+using NutricaoPortableApp.Model;
+using Xamarin.Forms;
+
+namespace NutricaoPortableApp.ViewModel
+{
+    public class CadastroRefeicaoViewModel : INotifyPropertyChanged
+    {
+        private string _descricao;
+        private double _calorias;
+        private RefeicaoDao _dao;
+        private ContentPage _page;
+        public ICommand SalvaRefeicao { get; private set; }
+
+        public string Descricao
+        {
+            get { return _descricao; }
+            set
+            {
+                if (value != _descricao)
+                {
+                    _descricao = value;
+                    OnPropertyChanged("Descricao");
+                }
+            }
+        }
+
+        public double Calorias
+        {
+            get { return _calorias; }
+            set
+            {
+                if (value != _calorias)
+                {
+                    _calorias = value;
+                    OnPropertyChanged("Calorias");
+                }
+            }
+        }
+
+        public CadastroRefeicaoViewModel(RefeicaoDao dao, ContentPage page)
+        {
+            _dao = dao;
+            _page = page;
+            SalvaRefeicao = new Command(() => {
+                Refeicao refeicao = new Refeicao(_descricao, _calorias);
+                _dao.Salvar(refeicao);
+                string msg = string.Format("A refeição {0} de {1} foi salva com successo.", _descricao, _calorias);
+                this._page.DisplayAlert("Salvar refeição", msg, "Ok");
+                
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string nome)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(nome));
+        }
+
+    }
+}
